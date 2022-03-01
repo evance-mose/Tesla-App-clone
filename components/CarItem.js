d232490064714432fabb0cc66df4a03b13efa45e
unknown <evasmose@gmail.com>
2022-03-01 11:39:31 +0200

    intial commit

---
+++ components/CarItem.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import React from 'react'
+
+const CarItem = () => {
+  return (
+    <View style={styles.carContainer}>
+    <ImageBackground source={require('../assets/images/ModelX.jpeg')} style={styles.image}>
+        <View style={styles.titles} >
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle} >Starting at $69,420</Text>
+        </View>
+    </ImageBackground>
+   
+  </View>
+  )
+}
+export default CarItem
+
+const styles = StyleSheet.create({
+    carContainer:{
+        width: '100%',
+        height: '100%',
+        resizeMode:"contain"       
+      },
+      titles:{
+        marginTop:"30%",
+        width: '100%',
+        alignItems: "center"  
+      },
+      title:{
+        fontSize:40,
+        fontWeight:'600'
+      },
+      subTitle:{
+        fontSize:16,
+        color: '#5c5e62'
+        
+      },
+      image:{
+        width: '100%',
+        height: '100%',
+      }
+})
